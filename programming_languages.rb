c878f15d39537ccbbd7fbdd792c7357f488e16ba
Yonas Fesehatsion <yone920@gmail.com>
2019-04-30 09:31:14 -0400

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,61 @@
+
+def languages  
+  {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+}
+}
+end
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  new_hash[:ruby] = languages[:oo][:ruby]
+  new_hash[:ruby][:style] = []
+  new_hash[:ruby][:style] << :oo
+  new_hash[:javascript] = languages[:oo][:javascript]
+  new_hash[:javascript][:style] = []
+  new_hash[:javascript][:style] << :oo << :functional
+  new_hash[:python] = languages[:oo][:python]
+  new_hash[:python][:style] = []
+  new_hash[:python][:style] << :oo
+  new_hash[:java] = languages[:oo][:java]
+  new_hash[:java][:style] = []
+  new_hash[:java][:style] << :oo
+  new_hash[:clojure] = languages[:functional][:clojure]
+  new_hash[:clojure][:style] = []
+  new_hash[:clojure][:style] << :functional
+  new_hash[:erlang] = languages[:functional][:erlang]
+  new_hash[:erlang][:style] = []
+  new_hash[:erlang][:style] << :functional
+  new_hash[:scala] = languages[:functional][:scala]
+  new_hash[:scala][:style] = []
+  new_hash[:scala][:style] << :functional
+
+
+  new_hash
 end
