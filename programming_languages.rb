d3bed49efea1bf85856e323da9b01918249b7125
Fanqiang Meng <fanmeng0417@gmail.com>
2019-05-01 04:08:14 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,19 @@
+require "pry"
 def reformat_languages(languages)
   # your code here
+  languagesBytype = {}
+  languages.collect do |sty, langs|
+    langs.each do |lang, typ|
+      languagesBytype[lang] = typ
+      languagesBytype[lang][:style] = []
+    end
+  end
+  languages.collect do |sty, langs|
+    langs.each do |lang, typ|
+      languagesBytype[lang][:style] << sty
+    end
+  end
+  return languagesBytype
 end
+#first run take all the languages and push into languagesBytype
+#second run take styles and push into languagesBytype
